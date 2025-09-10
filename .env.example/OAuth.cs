09610174b14c6e8f600db1575063da23a884f0af
IAmKirbki <daimywisselink91@gmail.com>
2025-09-10 14:29:59 +0200

    fix: made the environment static, added new variables and renamed the folder to the correct one

--- .env.example/OAuth.cs
+++
@@ -1,9 +0,0 @@
-namespace MAD_Keuzedeel.env
-{
-    public class OAuth
-    {
-        public string ApplicationId = "ApplicationId";
-        public string SecretKey = "SecretKey";
-        public string Url = "Url";
-    }
-}
---
+++ env.example/OAuth.cs
@@ -0,0 +1,12 @@
+namespace MAD_Keuzedeel.env
+{
+    public static class OAuth
+    {
+        public const string ApplicationId = "your_application_id_here";
+        public const string SecretKey = "your_secret_key_here";
+        public const string AuthorizeUrl = "your_authorize_url_here";
+        public const string TokenUrl = "your_token_url_here";
+        public const string UserInfoUrl = "your_user_info_url_here";
+        public const string RedirectUri = "your_redirect_uri_here";
+    }
+}
