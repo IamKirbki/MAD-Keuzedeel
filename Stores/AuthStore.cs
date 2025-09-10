eef47842b6deb42e73a88f36343da1154da39b60
IAmKirbki <daimywisselink91@gmail.com>
2025-09-10 14:37:56 +0200

    perf: improved stores performance by serializing and deserializing when storing data

--- Stores/AuthStore.cs
+++ Stores/AuthStore.cs
@@ -9,53 +9,9 @@ namespace MAD_Keuzedeel.Stores
 {
     class AuthStore : IStore<Auth>
     {
-        public static async Task<Auth?> Load()
-        {
-            return new Auth
-            {
-                id_token = await SecureStorage.Default.GetAsync("auth_data_id_token"),
-                access_token = await SecureStorage.Default.GetAsync("auth_data_access_token"),
-                refresh_token = await SecureStorage.Default.GetAsync("auth_data_refresh_token"),
-                scope = await SecureStorage.Default.GetAsync("auth_data_scope"),
-                token_type = await SecureStorage.Default.GetAsync("auth_data_token_type"),
-                expires_in = int.Parse(await SecureStorage.Default.GetAsync("auth_data_expires_in") ?? "0"),
-                created_at = int.Parse(await SecureStorage.Default.GetAsync("auth_data_created_at") ?? "0"),
-            };
-        }
-
-        public static async Task Store(Auth auth)
-        {
-            await SecureStorage.Default.SetAsync("auth_data_id_token", auth.id_token);
-            await SecureStorage.Default.SetAsync("auth_data_access_token", auth.access_token);
-            await SecureStorage.Default.SetAsync("auth_data_refresh_token", auth.refresh_token);
-            await SecureStorage.Default.SetAsync("auth_data_scope", auth.scope);
-            await SecureStorage.Default.SetAsync("auth_data_token_type", auth.token_type);
-            await SecureStorage.Default.SetAsync("auth_data_expires_in", auth.expires_in.ToString());
-            await SecureStorage.Default.SetAsync("auth_data_created_at", auth.created_at.ToString());
-        }
-
-        public static async Task Delete()
-        {
-            var keys = new List<string>
-            {
-                "auth_data_id_token",
-                "auth_data_access_token",
-                "auth_data_refresh_token",
-                "auth_data_scope",
-                "auth_data_token_type",
-                "auth_data_expires_in",
-                "auth_data_created_at"
-            };
-
-            foreach (var key in keys)
-            {
-                SecureStorage.Remove(key);
-            }
-        }
-
-        public static Task Update(Auth auth)
-        {
-            return Delete().ContinueWith(t => Store(auth));
-        }
+        public static async Task<Auth?> Load() => JsonConvert.DeserializeObject<Auth>(await SecureStorage.Default.GetAsync("auth_data")) ?? null;
+        public static async Task Store(Auth auth) => await SecureStorage.Default.SetAsync("auth_data", JsonConvert.SerializeObject(auth));
+        public static async Task Delete() => await SecureStorage.Remove("auth_data");
+        public static Task Update(Auth auth) => Delete().ContinueWith(t => Store(auth));
     }
 }
--- Stores/UserStore.cs
+++ Stores/UserStore.cs
@@ -9,44 +9,9 @@ namespace MAD_Keuzedeel.Stores
 {
     class UserStore : IStore<User>
     {
-        public static async Task<User?> Load()
-        {
-            return new User
-            {
-                id = int.Parse(await SecureStorage.Default.GetAsync("user_data_id") ?? "0"),
-                name = await SecureStorage.Default.GetAsync("user_data_name"),
-                email = await SecureStorage.Default.GetAsync("user_data_email"),
-                username = await SecureStorage.Default.GetAsync("user_data_username"),
-                avatar_url = await SecureStorage.Default.GetAsync("user_data_avatar_url")
-            };
-        }
-        public static async Task Store(User user)
-        {
-            await SecureStorage.SetAsync("user_data_id", user.id.ToString());
-            await SecureStorage.SetAsync("user_data_name", user.name ?? "");
-            await SecureStorage.SetAsync("user_data_email", user.email ?? "");
-            await SecureStorage.SetAsync("user_data_username", user.username ?? "");
-            await SecureStorage.SetAsync("user_data_avatar_url", user.avatar_url ?? "");
-        }
-
-        public static async Task Delete()
-        {
-            var keys = new List<string>
-            {
-                "user_data_id",
-                "user_data_name",
-                "user_data_email",
-                "user_data_username",
-                "user_data_avatar_url"
-            };
-            foreach (var key in keys)
-            {
-                SecureStorage.Remove(key);
-            }
-        }
-        public static Task Update(User user)
-        {
-            return Delete().ContinueWith(t => Store(user));
-        }
+        public static async Task<User?> Load() => JsonConvert.DeserializeObject<User>(await SecureStorage.Default.GetAsync("user_data")) ?? null;
+        public static async Task<User?> Store(User user) => await SecureStorage.Default.SetAsync("user_data", JsonConvert.SerializeObject(user));
+        public static async Task Delete() => await SecureStorage.Remove("user_data");
+        public static Task Update(User user) => Delete().ContinueWith(t => Store(user));
     }
 }
